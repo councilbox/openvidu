f6b6b97f75e19b24b3ddcea6bdf1c0c07ba8c508
Alex <alex@streaminggalicia.com>
2020-04-15 09:23:54 +0200

    Recording docker image only for broadcast by rtmp

--- openvidu-server/docker/openvidu-recording/create_image.sh
+++ openvidu-server/docker/openvidu-recording/create_image.sh
@@ -1,1 +1,1 @@
-docker build --rm -t councilbox/rtmp-recording .
+docker build --rm -t councilbox/rtmp-broadcast .
--- openvidu-server/docker/openvidu-recording/entrypoint.sh
+++ openvidu-server/docker/openvidu-recording/entrypoint.sh
@@ -12,37 +12,23 @@ else
 ### Variables ###
 
 URL=${URL:-https://councilbox.com}
-ONLY_VIDEO=${ONLY_VIDEO:-false}
 RESOLUTION=${RESOLUTION:-1920x1080}
 FRAMERATE=${FRAMERATE:-25}
 WIDTH="$(cut -d'x' -f1 <<< $RESOLUTION)"
 HEIGHT="$(cut -d'x' -f2 <<< $RESOLUTION)"
-VIDEO_ID=${VIDEO_ID:-video}
-VIDEO_NAME=${VIDEO_NAME:-video}
-VIDEO_FORMAT=${VIDEO_FORMAT:-mp4}
-RECORDING_JSON="${RECORDING_JSON}"
 RTMP_URL=${RTMP_URL:-false}
 
 export URL
-export ONLY_VIDEO
 export RESOLUTION
 export FRAMERATE
 export WIDTH
 export HEIGHT
-export VIDEO_ID
-export VIDEO_NAME
-export VIDEO_FORMAT
-export RECORDING_JSON
 export RTMP_URL
 
 ### Show rtmp url
 echo $RTMP_URL
-
-### Store Recording json data ###
-
-mkdir /recordings/$VIDEO_ID
-chmod 777 /recordings/$VIDEO_ID
-echo $RECORDING_JSON > /recordings/$VIDEO_ID/.recording.$VIDEO_ID
+### Show url to stream
+echo $URL
 
 ### Get a free display identificator ###
 
@@ -75,58 +61,11 @@ touch xvfb.log
 chmod 777 xvfb.log
 xvfb-run --server-num=${DISPLAY_NUM} --server-args="-ac -screen 0 ${RESOLUTION}x24 -noreset" google-chrome --start-maximized --no-sandbox --test-type --disable-infobars --window-size=$WIDTH,$HEIGHT --window-position=0,0 --no-first-run --ignore-certificate-errors --autoplay-policy=no-user-gesture-required --kiosk $URL &> xvfb.log &
 touch stop
-chmod 777 /recordings
 sleep 2
 
 if [[ "$RTMP_URL" != false ]]; then
   echo "Send rtmp"
   <./stop ffmpeg -y -f alsa -i pulse -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:a aac -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE -f flv $RTMP_URL
-  ### Generate false video report file ###
-  ##ffprobe -v quiet -print_format json -show_format -show_streams /recordings/fake.mp4 > /recordings/$VIDEO_ID/$VIDEO_ID.info
 else
-  if [[ "$ONLY_VIDEO" == true ]]
-    then
-      # Do not record audio
-      echo "Record only video"
-      <./stop ffmpeg -y -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE "/recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT"     
-  else
-     # Record audio  ("-f alsa -i pulse [...] -c:a aac")
-     echo "Record video + audio"
-     <./stop ffmpeg -y -f alsa -i pulse -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:a aac -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE "/recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT"
-  fi
-
-  ### Generate video report file ###
-  ffprobe -v quiet -print_format json -show_format -show_streams /recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT > /recordings/$VIDEO_ID/$VIDEO_ID.info
-
-  ### Update Recording json data ###
-
-  TMP=$(mktemp /recordings/$VIDEO_ID/.$VIDEO_ID.XXXXXXXXXXXXXXXXXXXXXXX.json)
-  INFO=$(cat /recordings/$VIDEO_ID/$VIDEO_ID.info | jq '.')
-  HAS_AUDIO_AUX=$(echo $INFO | jq '.streams[] | select(.codec_type == "audio")')
-  if [ -z "$HAS_AUDIO_AUX" ]; then HAS_AUDIO=false; else HAS_AUDIO=true; fi
-  HAS_VIDEO_AUX=$(echo $INFO | jq '.streams[] | select(.codec_type == "video")')
-  if [ -z "$HAS_VIDEO_AUX" ]; then HAS_VIDEO=false; else HAS_VIDEO=true; fi
-  SIZE=$(echo $INFO | jq '.format.size | tonumber')
-  DURATION=$(echo $INFO | jq '.format.duration | tonumber')
-
-  if [[ "$HAS_AUDIO" == false && "$HAS_VIDEO" == false ]]
-    then
-    STATUS="failed"
-  else
-    STATUS="stopped"
-  fi
-
-  jq -c -r ".hasAudio=$HAS_AUDIO | .hasVideo=$HAS_VIDEO | .duration=$DURATION | .size=$SIZE | .status=\"$STATUS\"" "/recordings/$VIDEO_ID/.recording.$VIDEO_ID" > $TMP && mv $TMP /recordings/$VIDEO_ID/.recording.$VIDEO_ID
-
-  ### Generate video thumbnail ###
-
-  MIDDLE_TIME=$(ffmpeg -i /recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT 2>&1 | grep Duration | awk '{print $2}' | tr -d , | awk -F ':' '{print ($3+$2*60+$1*3600)/2}')
-  THUMBNAIL_HEIGHT=$((480*$HEIGHT/$WIDTH))
-  ffmpeg -ss $MIDDLE_TIME -i /recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT -vframes 1 -s 480x$THUMBNAIL_HEIGHT /recordings/$VIDEO_ID/$VIDEO_ID.jpg
-
-  ### Change permissions to all generated files ###
-
-  sudo chmod -R 777 /recordings/$VIDEO_ID
-
-  fi
+  echo "Rtmp sending failed, rtmp address not received"
 fi
